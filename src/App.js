531957b3f69c333bd0d4be8889844f7c80230aa7
Aaqib Javed <aqib.javed@codeavour.com>
2020-05-24 16:23:04 +0500

    Adding routing for shop page - just basic routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css'
-import Homepage from './pages/homepage/homepage.component';
+import { Route, Switch, Link } from 'react-router-dom'
+import Homepage from './pages/homepage/homepage.component'
 
+function HatsPage() {
+  return (
+    <h1>Hello hats</h1>
+  )
+}
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './menu-item.styles.scss'
+import { withRouter } from 'react-router-dom'
 
-const MenuItem = ({ title, imageUrl, size }) => {
+const MenuItem = ({ title, imageUrl, size, linkUrl, history, match }) => {
     return (
-        <div className={`${size} menu-item`}>
-        
+        <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
+
             <div style={{
                 backgroundImage: `url(${imageUrl})`
             }}  className="background-image"></div>
@@ -18,4 +19,4 @@ const MenuItem = ({ title, imageUrl, size }) => {
     )
 }
 
-export default MenuItem
+export default withRouter(MenuItem)
